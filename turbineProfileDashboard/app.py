c93baec1a1c97ada195a5918306649b4787b992c
AntohaProgrammist <64589585+AntohaProgrammist@users.noreply.github.com>
2020-05-04 18:10:39 +0300

    ITS ALIVE

--- turbineProfileDashboard/app.py
+++ turbineProfileDashboard/app.py
@@ -2,10 +2,20 @@ import dash
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
-
+from dash.dependencies import Input, Output
+from main import Blade
+import plotly.graph_objects as go
+from random import randint
+from flask import Flask
 
 stylesheets = ['https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css']
-app = dash.Dash(external_stylesheets=stylesheets)
+server = Flask(__name__)
+app = dash.Dash(
+    __name__,
+    external_stylesheets=stylesheets,
+    server=server
+    # external_stylesheets=[dbc.themes.SKETCHY]
+)
 
 app.layout = dbc.Container([
     dbc.Row([
@@ -21,35 +31,140 @@ app.layout = dbc.Container([
         dbc.Col([
             html.H6("Параметры"),
             html.Hr(),
-            html.P("Скорость в относительном движении перед рабочей лопаткой, [м/с]"),
-            dcc.Input(id='w_1', value=314, type="number"),
-            html.P("Скорость в относительном движении за рабочей лопаткой, [м/с]"),
-            dcc.Input(id='w_2', value=371, type="number"),
-            html.P("Хорда лопатки, [мм]"),
-            dcc.Input(id='B', value=30, type="number"),
-            html.P("Угол в относительном движении в сечении перед рабочей лопаткой, [град.]"),
-            dcc.Input(id='beta_1', value=38, type="number"),
-            html.P("Угол в относительном движении в сечении за рабочей лопаткой, [град.]"),
-            dcc.Input(id='beta_2', value=30, type="number"),
-            html.P("Установочный угол, [град.]"),
-            dcc.Input(id='beta_y', value=80, type="number"),
+            html.P("Хорда лопатки, [мм]", style={"margin-bottom": "0"}),
+            dcc.Input(id='B', value=randint(20, 40), type="number", className="form-control"),
+            html.P("Угол в относительном движении в сечении перед рабочей лопаткой, [град.]", style={"margin-bottom": "0"}),
+            dcc.Input(id='beta_1', value=randint(15, 50), type="number", className="form-control"),
+            html.P("Угол в относительном движении в сечении за рабочей лопаткой, [град.]", style={"margin-bottom": "0"}),
+            dcc.Input(id='beta_2', value=randint(15, 50), type="number", className="form-control"),
+            html.P("Установочный угол, [град.]", style={"margin-bottom": "0"}),
+            dcc.Input(id='beta_y', value=randint(45, 90), type="number", className="form-control"),
 
-            html.P("Угол раскрытия на входе в лопатка, [град.]"),
-            dcc.Input(id='gamma_1', value=30, type="number"),
-            html.P("Угол раскрытия на выходе в лопатка, [град.]"),
-            dcc.Input(id='gamma_2', value=4, type="number"),
-            html.P("Радиус скругления входной кромки, [мм]"),
-            dcc.Input(id='radius_1', value=4, type="number"),
-            html.P("Радиус скругления выходной кромки, [мм]"),
-            dcc.Input(id='radius_2', value=4, type="number"),
-        ], width=4),
+            html.P("Угол раскрытия на входе в лопатка, [град.]", style={"margin-bottom": "0"}),
+            dcc.Input(id='gamma_1', value=randint(8, 30), type="number", className="form-control"),
+            html.P("Угол раскрытия на выходе в лопатка, [град.]", style={"margin-bottom": "0"}),
+            dcc.Input(id='gamma_2', value=randint(4, 8), type="number", className="form-control"),
+            html.P("Радиус скругления входной кромки, [мм]", style={"margin-bottom": "0"}),
+            dcc.Input(id='radius_1', value=1, className="form-control"),
+            html.P("Радиус скругления выходной кромки, [мм]", style={"margin-bottom": "0"}),
+            dcc.Input(id='radius_2', value=0.5, className="form-control"),
+        ], width=4, style={'height': '100%'}),
 
         dbc.Col([
             html.H6("Лопатка"),
-            # График
-        ], width=True),
+            html.Hr(),
+            dcc.Graph(id='display', style={
+                'height': '70vh'
+            })
+        ], width=8, align="top"),
 
-    ], align="end"),
-], fluid=True)
+    ], align="top"),
 
-app.run_server()
+    dbc.Row([
+        dbc.Col([
+            html.Hr(),
+            html.P(["Если хотите смешной анекдот про лопатку, не пишите мне. ",
+                    html.A("telegram", href="https://t.me/antonkkkkk")]),
+            html.Hr(),
+        ], width=True, align="end"),
+    ], justify='center'),
+], fluid=False)
+
+
+@app.callback(
+    [Output('display', 'figure')],
+    [
+        Input('B', 'value'), Input('beta_1', 'value'),
+        Input('beta_2', 'value'), Input('beta_y', 'value'),
+        Input('gamma_1', 'value'), Input('gamma_2', 'value'),
+        Input('radius_1', 'value'), Input('radius_2', 'value')
+    ]
+)
+def main_calc(
+        B, beta_1, beta_2, beta_y,
+        gamma_1, gamma_2, radius_1, radius_2
+):
+    w_1 = 10
+    w_2 = 10
+    beta_y = 180 - beta_y
+    lopatka = Blade(
+        B,
+        beta_1,
+        beta_2,
+        beta_y,
+        gamma_1,
+        gamma_2,
+        w_1,
+        w_2,
+        float(radius_1),
+        float(radius_2)
+    )
+    lopatka.calc()
+
+    x, y = lopatka.return_coords()
+
+    fig = dict({
+        "data": [
+            {
+                "type": "scatter",
+                "x": x.tolist(),
+                "y": y.tolist()
+            }
+        ],
+        "layout": {
+            # "title": {"text": "A Figure Specified By Python Dictionary"},
+            "xaxis": dict(range=[-30, 30]),
+            "yaxis": dict(scaleanchor="x", scaleratio=1),  # dict(range=[-30, 30]),
+            "autosize": True,
+            "source": "https://images.plot.ly/language-icons/api-home/python-logo.png",
+        }
+    })
+    fig = go.Figure(
+        data=[
+            {
+                "type": "scatter",
+                "x": x.tolist(),
+                "y": y.tolist()
+            }
+        ],
+        layout={
+            # "title": {"text": "A Figure Specified By Python Dictionary"},
+            "xaxis": dict(range=[-30, 30]),
+            "yaxis": dict(scaleanchor="x", scaleratio=1),  # dict(range=[-30, 30]),
+            "autosize": True,
+            # "source": "https://images.plot.ly/language-icons/api-home/python-logo.png",
+        }
+    )
+    # fig.add_layout_image(
+    #     dict(
+    #         xref="x",
+    #         yref="y",
+    #         layer="below",
+    #         sizing="stretch",
+    #         opacity=.3,
+    #         source="images/koteika.jpg"
+    #     )
+    # )
+    # fig.add_layout_image(
+    #     dict(
+    #         source="images/koteika.png",
+    #         xref="x",
+    #         yref="y",
+    #         x=-5,
+    #         y=3,
+    #         sizex=1,
+    #         sizey=1,
+    #         sizing="stretch",
+    #         opacity=0.5,
+    #         layer="below"
+    #     )
+    # )
+    # fig.update_layout(
+    #     template="plotly_white"
+    # )
+
+    return [fig]
+
+
+app.title = 'Лопатка'
+# app.run_server()
--- turbineProfileDashboard/main.py
+++ turbineProfileDashboard/main.py
@@ -6,171 +6,227 @@ from funcs import bezier_curve as bezier
 
 # Хорда
 B = 30  # мм
-
 # Угол в относительном движении в сечении перед рабочей лопаткой
 beta_1 = 38  # градусов
 # Угол в относительном движении в сечении за рабочей лопаткой
 beta_2 = 34  # градусов
 # Установочный угол
 beta_y = 180  # - (beta_1 + beta_2)/2
-
 # Угол раскрытия лопатки
 gamma_1 = 30  # градусов
 # Угол закртия лопатки
 gamma_2 = 4  # градусов
-
 # Скорости в относительном движении перед и за рабочим колесом турбины
 w_1 = 314 / 100  # м/с
 w_2 = 371 / 100  # м/с
-
 # Радиусы скргуления входной и выходной кромок
 radius_1 = 0.04 * B
 radius_2 = 0.01 * B
 
-# Координаты начала построений
-X, Y = 0, 0
 
-# Перевод в радианы
-beta_1 = np.radians(beta_1)
-beta_2 = np.radians(beta_2)
-beta_y = np.radians(beta_y)
-gamma_1 = np.radians(gamma_1)
-gamma_2 = np.radians(gamma_2)
+class Blade:
+    """ docsting """
 
-# Координаты точки рабочего колеса на входе
-x_1 = X
-y_1 = Y
-# Координаты н и угла наклона потижней точки рабочего колеса на входе с учетом скоростиока
-x_1w = x_1 + w_1 * np.cos(beta_1)
-y_1w = y_1 + w_1 * np.sin(beta_1)
+    def __init__(
+            self,
+            B,
+            beta_1,
+            beta_2,
+            beta_y,
+            gamma_1,
+            gamma_2,
+            w_1,
+            w_2,
+            radius_1,
+            radius_2
+    ):
+        self.B = B
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.beta_y = beta_y
+        self.gamma_1 = gamma_1
+        self.gamma_2 = gamma_2
+        self.w_1 = w_1
+        self.w_2 = w_2
+        self.radius_1 = radius_1
+        self.radius_2 = radius_2
 
-x_1radius = x_1 + radius_1 * np.cos(beta_1)
-y_1radius = y_1 + radius_1 * np.sin(beta_1)
+        self.x, self.y = None, None
+        self.center_of_mass = None
 
-# Это точка l
-x_1_spinka = x_1 - radius_1 * np.sin(beta_1 - gamma_1 / 2)
-y_1_spinka = y_1 + radius_1 * np.cos(beta_1 - gamma_1 / 2)
-x_1w_spinka = x_1_spinka + w_1 * np.cos(beta_1 - gamma_1 / 2)
-y_1w_spinka = y_1_spinka + w_1 * np.sin(beta_1 - gamma_1 / 2)
+        pass
 
-# Это точка g
-x_1_korytse = x_1 + radius_1 * np.sin(beta_1 + gamma_1 / 2)
-y_1_korytse = y_1 - radius_1 * np.cos(beta_1 + gamma_1 / 2)
-x_1w_korytse = x_1_korytse + w_1 * np.cos(beta_1 + gamma_1 / 2)
-y_1w_korytse = y_1_korytse + w_1 * np.sin(beta_1 + gamma_1 / 2)
+    def calc(self):
+        # Координаты начала построений
+        x_0, y_0 = 0, 0
 
-x_2 = x_1 - B * np.cos(beta_y)
-y_2 = y_1 - B * np.sin(beta_y)
+        # Перевод в радианы
+        beta_1 = np.radians(self.beta_1)
+        beta_2 = np.radians(self.beta_2)
+        beta_y = np.radians(self.beta_y)
+        gamma_1 = np.radians(self.gamma_1)
+        gamma_2 = np.radians(self.gamma_2)
 
-x_2w = x_2 + w_2 * np.cos(beta_2)
-y_2w = y_2 - w_2 * np.sin(beta_2)
+        # Координаты точки рабочего колеса на входе
+        x_1 = x_0
+        y_1 = y_0
+        # Координаты н и угла наклона потижней точки рабочего колеса на входе с учетом скоростиока
+        x_1w = x_1 + self.w_1 * np.cos(beta_1)
+        y_1w = y_1 + self.w_1 * np.sin(beta_1)
 
-x_2radius = x_2 + radius_2 * np.cos(beta_2)
-y_2radius = y_2 - radius_2 * np.sin(beta_2)
+        x_1radius = x_1 + self.radius_1 * np.cos(beta_1)
+        y_1radius = y_1 + self.radius_1 * np.sin(beta_1)
 
-x_2_spinka = x_2 - radius_2 * np.sin(beta_2 - gamma_2 / 2)
-y_2_spinka = y_2 - radius_2 * np.cos(beta_2 - gamma_2 / 2)
+        # Это точка l
+        x_1_spinka = x_1 - self.radius_1 * np.sin(beta_1 - gamma_1 / 2)
+        y_1_spinka = y_1 + self.radius_1 * np.cos(beta_1 - gamma_1 / 2)
+        x_1w_spinka = x_1_spinka + self.w_1 * np.cos(beta_1 - gamma_1 / 2)
+        y_1w_spinka = y_1_spinka + self.w_1 * np.sin(beta_1 - gamma_1 / 2)
 
-x_2w_spinka = x_2_spinka + w_2 * np.cos(beta_2 - gamma_2 / 2)
-y_2w_spinka = y_2_spinka - w_2 * np.sin(beta_2 - gamma_2 / 2)
+        # Это точка g
+        x_1_korytse = x_1 + self.radius_1 * np.sin(beta_1 + gamma_1 / 2)
+        y_1_korytse = y_1 - self.radius_1 * np.cos(beta_1 + gamma_1 / 2)
+        x_1w_korytse = x_1_korytse + self.w_1 * np.cos(beta_1 + gamma_1 / 2)
+        y_1w_korytse = y_1_korytse + self.w_1 * np.sin(beta_1 + gamma_1 / 2)
 
-x_2_korytse = x_2 + radius_2 * np.sin(beta_2 + gamma_2 / 2)
-y_2_korytse = y_2 + radius_2 * np.cos(beta_2 + gamma_2 / 2)
+        x_2 = x_1 - self.B * np.cos(beta_y)
+        y_2 = y_1 - self.B * np.sin(beta_y)
 
-x_2w_korytse = x_2_korytse + w_2 * np.cos(beta_2 + gamma_2 / 2)
-y_2w_korytse = y_2_korytse - w_2 * np.sin(beta_2 + gamma_2 / 2)
+        x_2w = x_2 + self.w_2 * np.cos(beta_2)
+        y_2w = y_2 - self.w_2 * np.sin(beta_2)
 
-x_tochka_spinka, y_tochka_spinka = tochka(
-    [x_1_spinka, x_1w_spinka],
-    [y_1_spinka, y_1w_spinka],
-    [x_2_spinka, x_2w_spinka],
-    [y_2_spinka, y_2w_spinka]
-)
+        x_2radius = x_2 + self.radius_2 * np.cos(beta_2)
+        y_2radius = y_2 - self.radius_2 * np.sin(beta_2)
 
-x_tochka_korytse, y_tochka_korytse = tochka(
-    [x_1_korytse, x_1w_korytse],
-    [y_1_korytse, y_1w_korytse],
-    [x_2_korytse, x_2w_korytse],
-    [y_2_korytse, y_2w_korytse]
-)
+        x_2_spinka = x_2 - self.radius_2 * np.sin(beta_2 - gamma_2 / 2)
+        y_2_spinka = y_2 - self.radius_2 * np.cos(beta_2 - gamma_2 / 2)
 
-points_for_spinka = [
-    [x_1_spinka, y_1_spinka, 0],
-    [x_tochka_spinka, y_tochka_spinka, 0],
-    [x_2_spinka, y_2_spinka, 0],
-]
-spinka = bezier(points_for_spinka, 50)
-# ТЕСТИРОВАНИЕ СПЛАЙНА В ФРИКАДЕ
+        x_2w_spinka = x_2_spinka + self.w_2 * np.cos(beta_2 - gamma_2 / 2)
+        y_2w_spinka = y_2_spinka - self.w_2 * np.sin(beta_2 - gamma_2 / 2)
 
-# КОНЕЦ ТЕСТИРОВАНИЯ
+        x_2_korytse = x_2 + self.radius_2 * np.sin(beta_2 + gamma_2 / 2)
+        y_2_korytse = y_2 + self.radius_2 * np.cos(beta_2 + gamma_2 / 2)
 
-# C:\Users\asea2\Desktop\PycharmProjects\Prikolno\freecadfiles
-points_for_korytse = [
-    [x_1_korytse, y_1_korytse, 0],
-    [x_tochka_korytse, y_tochka_korytse, 0],
-    [x_2_korytse, y_2_korytse, 0],
-]
-korytse = bezier(points_for_korytse, 50)
+        x_2w_korytse = x_2_korytse + self.w_2 * np.cos(beta_2 + gamma_2 / 2)
+        y_2w_korytse = y_2_korytse - self.w_2 * np.sin(beta_2 + gamma_2 / 2)
 
-x_1p1_spinka = x_1_spinka + radius_1 * np.cos(beta_1 - gamma_1 / 2)
-y_1p1_spinka = y_1_spinka + radius_1 * np.sin(beta_1 - gamma_1 / 2)
-x_1p3 = x_1 + 1.5*radius_1 * np.cos(beta_1)
-y_1p3 = y_1 + 1.5*radius_1 * np.sin(beta_1)
-x_1p2_korytse = x_1_korytse + radius_1 * np.cos(beta_1 + gamma_1 / 2)
-y_1p2_korytse = y_1_korytse + radius_1 * np.sin(beta_1 + gamma_1 / 2)
-points_for_radius1 = [
-    [x_1_spinka, y_1_spinka, 0],
-    [x_1p1_spinka, y_1p1_spinka, 0],
-    [x_1p3, y_1p3, 0],
-    [x_1p2_korytse, y_1p2_korytse, 0],
-    [x_1_korytse, y_1_korytse, 0],
-]
-radius1 = bezier(points_for_radius1, 20)
+        x_tochka_spinka, y_tochka_spinka = tochka(
+            [x_1_spinka, x_1w_spinka],
+            [y_1_spinka, y_1w_spinka],
+            [x_2_spinka, x_2w_spinka],
+            [y_2_spinka, y_2w_spinka]
+        )
 
-x_2p1_spinka = x_2_spinka + radius_2 * np.cos(beta_2 - gamma_2 / 2)
-y_2p1_spinka = y_2_spinka - radius_2 * np.sin(beta_2 - gamma_2 / 2)
-x_2p3 = x_2 + 1.5*radius_2 * np.cos(beta_2)
-y_2p3 = y_2 - 1.5*radius_2 * np.sin(beta_2)
-x_2p2_korytse = x_2_korytse + radius_2 * np.cos(beta_2 + gamma_2 / 2)
-y_2p2_korytse = y_2_korytse - radius_2 * np.sin(beta_2 + gamma_2 / 2)
-points_for_radius2 = [
-    [x_2_spinka, y_2_spinka, 0],
-    [x_2p1_spinka, y_2p1_spinka, 0],
-    [x_2p3, y_2p3, 0],
-    [x_2p2_korytse, y_2p2_korytse, 0],
-    [x_2_korytse, y_2_korytse, 0],
-]
-radius2 = bezier(points_for_radius2, 20)
+        x_tochka_korytse, y_tochka_korytse = tochka(
+            [x_1_korytse, x_1w_korytse],
+            [y_1_korytse, y_1w_korytse],
+            [x_2_korytse, x_2w_korytse],
+            [y_2_korytse, y_2w_korytse]
+        )
 
-# Расчёт средней линии
-x_tochka_srednaya, y_tochka_srednaya = tochka(
-    [x_1w, x_1],
-    [y_1w, y_1],
-    [x_2w, x_2],
-    [y_2w, y_2]
-)
-points_for_srednaya = [
-    [x_1radius, y_1radius, 0],
-    [x_tochka_srednaya, y_tochka_srednaya, 0],
-    [x_2radius, y_2radius, 0],
-]
-srednaya = bezier(points_for_srednaya, 50)
+        points_for_spinka = [
+            [x_1_spinka, y_1_spinka, 0],
+            [x_tochka_spinka, y_tochka_spinka, 0],
+            [x_2_spinka, y_2_spinka, 0],
+        ]
+        spinka = bezier(points_for_spinka, 50)
 
-x, y = spinka
-x, y = np.append(x, np.flip(radius1[0])), np.append(y, np.flip(radius1[1]))
-x, y = np.append(x, np.flip(korytse[0])), np.append(y, np.flip(korytse[1]))
-x, y = np.append(x, radius2[0]), np.append(y, radius2[1])
+        points_for_korytse = [
+            [x_1_korytse, y_1_korytse, 0],
+            [x_tochka_korytse, y_tochka_korytse, 0],
+            [x_2_korytse, y_2_korytse, 0],
+        ]
+        korytse = bezier(points_for_korytse, 50)
 
-center_of_mass = [
-    np.sum(x * np.ones(len(x))) / np.sum(np.ones(len(x))),
-    np.sum(y * np.ones(len(y))) / np.sum(np.ones(len(y)))
-]
+        x_1p1_spinka = x_1_spinka + self.radius_1 * np.cos(beta_1 - gamma_1 / 2)
+        y_1p1_spinka = y_1_spinka + self.radius_1 * np.sin(beta_1 - gamma_1 / 2)
+        x_1p3 = x_1 + 1.5 * self.radius_1 * np.cos(beta_1)
+        y_1p3 = y_1 + 1.5 * self.radius_1 * np.sin(beta_1)
+        x_1p2_korytse = x_1_korytse + self.radius_1 * np.cos(beta_1 + gamma_1 / 2)
+        y_1p2_korytse = y_1_korytse + self.radius_1 * np.sin(beta_1 + gamma_1 / 2)
+        points_for_radius1 = [
+            [x_1_spinka, y_1_spinka, 0],
+            [x_1p1_spinka, y_1p1_spinka, 0],
+            [x_1p3, y_1p3, 0],
+            [x_1p2_korytse, y_1p2_korytse, 0],
+            [x_1_korytse, y_1_korytse, 0],
+        ]
+        radius1 = bezier(points_for_radius1, 20)
 
-x_array, y_array = x-center_of_mass[0], y-center_of_mass[1]
+        x_2p1_spinka = x_2_spinka + self.radius_2 * np.cos(beta_2 - gamma_2 / 2)
+        y_2p1_spinka = y_2_spinka - self.radius_2 * np.sin(beta_2 - gamma_2 / 2)
+        x_2p3 = x_2 + 1.5 * self.radius_2 * np.cos(beta_2)
+        y_2p3 = y_2 - 1.5 * self.radius_2 * np.sin(beta_2)
+        x_2p2_korytse = x_2_korytse + self.radius_2 * np.cos(beta_2 + gamma_2 / 2)
+        y_2p2_korytse = y_2_korytse - self.radius_2 * np.sin(beta_2 + gamma_2 / 2)
+        points_for_radius2 = [
+            [x_2_spinka, y_2_spinka, 0],
+            [x_2p1_spinka, y_2p1_spinka, 0],
+            [x_2p3, y_2p3, 0],
+            [x_2p2_korytse, y_2p2_korytse, 0],
+            [x_2_korytse, y_2_korytse, 0],
+        ]
+        radius2 = bezier(points_for_radius2, 20)
 
-sr_1x = srednaya[0] + B / 2-center_of_mass[0]
-sr_2x = srednaya[0] - B / 2-center_of_mass[0]
-sr_1y = srednaya[1]-center_of_mass[1]
-sr_2y = srednaya[1]-center_of_mass[1]
+        # Расчёт средней линии
+        x_tochka_srednaya, y_tochka_srednaya = tochka(
+            [x_1w, x_1],
+            [y_1w, y_1],
+            [x_2w, x_2],
+            [y_2w, y_2]
+        )
+        points_for_srednaya = [
+            [x_1radius, y_1radius, 0],
+            [x_tochka_srednaya, y_tochka_srednaya, 0],
+            [x_2radius, y_2radius, 0],
+        ]
+        srednaya = bezier(points_for_srednaya, 50)
 
-b = 1
+        # Основные координаты
+        x, y = spinka
+        x, y = np.append(x, np.flip(radius1[0])), np.append(y, np.flip(radius1[1]))
+        x, y = np.append(x, np.flip(korytse[0])), np.append(y, np.flip(korytse[1]))
+        x, y = np.append(x, radius2[0]), np.append(y, radius2[1])
+        self.x, self.y = x, y
+
+        center_of_mass = [
+            np.sum(x * np.ones(len(x))) / np.sum(np.ones(len(x))),
+            np.sum(y * np.ones(len(y))) / np.sum(np.ones(len(y)))
+        ]
+        self.center_of_mass = center_of_mass
+        x_array, y_array = x - center_of_mass[0], y - center_of_mass[1]
+
+        self.x, self.y = x_array, y_array
+
+        # sr_1x = srednaya[0] + self.B / 2 - center_of_mass[0]
+        # sr_2x = srednaya[0] - self.B / 2 - center_of_mass[0]
+        # sr_1y = srednaya[1] - center_of_mass[1]
+        # sr_2y = srednaya[1] - center_of_mass[1]
+
+        pass
+
+    def return_coords(self):
+        return [self.x, self.y]
+
+    def return_point(self):
+        # TODO
+        pass
+
+
+# lopatka = Blade(
+#     B,
+#     beta_1,
+#     beta_2,
+#     beta_y,
+#     gamma_1,
+#     gamma_2,
+#     w_1,
+#     w_2,
+#     radius_1,
+#     radius_2
+# )
+# lopatka.calc()
+
+# print(lopatka.return_coords())
+
+
